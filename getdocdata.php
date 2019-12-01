6812ac7dc8ffc20cb1b2876b40a6819582dce2b7
Zeel Shah <zshah26@uwo.ca>
2019-12-01 18:25:36 -0500

    changes to getdoctordata.php

--- getdocdata.php
+++ getdocdata.php
@@ -1,32 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Doctor's Info</title>
-	<link rel="stylesheet" type="text/css" href="doctordb.css">
-	<link href="https://fonts.google.com/specimen/Fjalla+One" rel-"stylesheet">
-	<script src="doctors.js"></script>
+	<title>Doctor's Database</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body>
-
+<script src="js.js"></script>
 <?php
 include 'connecttodb.php';
 ?>
 <h1>Doctor's Information:</h1>
 <ol>
 <?php
-   $doctorLicNum= $_POST["doctors"];
-   $query = 'SELECT * FROM doctor, hospital WHERE doctor.hosWorkAt=hospital.hosCode AND doctor.docLicNum="' . $docLicNum. '"';
+   $doctorLicenseNum= $_POST["doctors"];
+   $query = 'SELECT * FROM doctor, hospital WHERE doctor.hospID=hospital.hospCode AND doctor.licenseNum="' . $doctorLicenseNum. '"';
     $result=mysqli_query($connection,$query);
     if (!$result) {
          die("Database query failed.");
      }
     while ($row=mysqli_fetch_assoc($result)) {
         echo "<li>";
-        echo "<b>Name: </b>" . $row["firstName"] . " " . $row["lastName"] ." " . "</li>";  
-        echo "<b>License Number: </b>" . " " . $row["licenseNum"] . " " ."/li";
-        echo "<b>Specialtiy: </b>" . " " .  $row["specialty"] . " " ."</li>";
-        echo "<b>License Date: </b>" . " " .  $row[licenseDate] . " " . "</li>";
-        echo "<b>Hospital: </b>" . " " .  $row["hospName"] . "</li>";
+        echo "<b>Name: </b>" . $row["firstName"] . " " . $row["lastName"] ." " .  "<b>License Number: </b>" . " " . $row["licenseNum"] . " " . "<b>Specialtiy: </b>" . " " .  $row["specialty"] . " " .  "<b>License Date: </b>" . " " .  $row[licenseDate] . " " .  "<b>Hospital: </b>" . " " .  $row["hospName"] . "</li>";
 	echo '<img src="'.$row["docPicture"].'" height="60" width="60">';     
 }
      mysqli_free_result($result);
