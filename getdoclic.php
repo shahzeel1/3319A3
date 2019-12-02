404551b6a3335e6a70fa43f07e680510cb314465
Zeel Shah <zshah26@uwo.ca>
2019-12-02 15:52:34 -0500

    second section comments

--- getdoclic.php
+++ getdoclic.php
@@ -1,45 +1,49 @@
+<!-- This page gets the information of the doctor's who were licensed before the input date -->
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Doctor's License Page</title>
 	<link rel="stylesheet" type="text/css" href="doctordb.css">
-	<link href="https://fonts.google.com/specimen/Fjalla+One" rel-"stylesheet">
-	<script src="doctors.js"></script>
 </head>
 <body>
 <?php
-   include 'connecttodb.php';
+   include 'connecttodb.php';// connect to the db
 ?>
     <ul>
 <?php
-        $licDate= $_POST["licdate"];
+
+        $licDate= $_POST["licdate"]; // get the license date the user entered 
 
 echo"<h1>Doctor's Licensed Before ".$licDate."</h1>";
-$valid = validateDate($licDate);
+$valid = validateDate($licDate); // make sure thr date is valid 
         
-   if($valid)
+   if($valid) // if valid then proceed to outputting the info
    {
-   $query1= 'SELECT * FROM doctor WHERE licenseDate < "' .$licDate. '"';
-   $result=mysqli_query($connection,$query1);
-   if (!$result) {
+   
+   $query= 'SELECT * FROM doctor WHERE licenseDate < "' .$licDate. '"';// get the query 
+   $result=mysqli_query($connection,$query);// send the query into the db
+   // if there is no result it means the query failed
+    if (!$result) {
+
           die("Query failed.");
    }
-    while ($row=mysqli_fetch_assoc($result)) {
+    while ($row=mysqli_fetch_assoc($result)) { // output all the doctors' first name, last name, license num, and speciality 
         echo '<li>';
         echo "<b>Name: </b>" . $row["firstName"] . " " . $row["lastName"] ." " .  "<b>License No.: </b>" . " " . $row["docLicNum"] . " " . "<b>Speciality: </b>" . " " . $row["speciality"] . "</li>"; 
      }
-   mysqli_close($connection);
+   mysqli_close($connection); // clsoe the connection to the db
+
    }
-else 
+else // date was not the right format
 {
 echo "Invalid Date Entry";
-}        
+}    
+        // fucntion checks if the date format is correct
     function validateDate($date, $format='Y-m-d')
 {
     $d = DateTime::createFromFormat($format, $date);
     
     return $d && $d->format($format) == $date;
-        
 }
 ?>
 </ul>
