38f9a21a22b406a03b7c473821ca148aadf6d600
Zeel Shah <zshah26@uwo.ca>
2019-12-02 01:09:50 -0500

    changes to list hospitals

--- getpatientinfo.php
+++ getpatientinfo.php
@@ -21,8 +21,8 @@
    $result1=mysqli_query($connection,$query);
    if (!$result1) {
 	die("Database query failed. Patient does not exist.");
-   $query= 'SELECT patient.firstname AS p_fname, patient.lastname AS p_lname, patient.ohip AS p_OHIP, doctor.firstName AS d_fname, doctor.lastName AS d_lname  FROM patient,treats,doctor  WHERE patient.ohip='.$ohipint. 'AND treats.ohip='.$ohipint.'AND treats.docLicNum=doctor.docLicNum;';
-   $result=mysqli_query($connection,$query);
+   $query2= 'SELECT patient.firstname AS p_fname, patient.lastname AS p_lname, patient.ohip AS p_OHIP, doctor.firstName AS d_fname, doctor.lastName AS d_lname  FROM patient,treats,doctor  WHERE patient.ohip='.$ohipint. 'AND treats.ohip='.$ohipint.'AND treats.docLicNum=doctor.docLicNum;';
+   $result=mysqli_query($connection,$query2);
    if (!$result) {
           die("Patient does not exist");
    }
